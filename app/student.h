d9f5cf64c5db1f1955f6f7a8e6759626dfae8da1
phadyniak <public.hadyniak@gmail.com>
2018-05-18 11:00:48 +0300

    student

--- app/student.h
+++ app/student.h
@@ -1,1 +1,12 @@
 #pragma once
+
+typedef struct Student Student;
+struct Student {
+    int id;
+    char name[100];
+    float score;
+};
+
+Student * Student_new(int id, const char * name, float score);
+Student * Student_newCopy(Student * student);
+void Student_free(Student * self);
